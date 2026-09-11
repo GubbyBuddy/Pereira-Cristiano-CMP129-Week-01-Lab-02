aca28b06d4caea8377a28f0c7a54c885cb93b5fb
Gubbybuddy <pereira.cristiano@student.ccm.edu>
2026-09-11 13:49:35 -0400

    commit 1

--- Patterns.java
+++ Patterns.java
@@ -1,5 +1,25 @@
-package Labs.Week_1.scr;
+import java.util.Scanner;
 
 public class Patterns {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String chart = ""; 
+        
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Please enter todays sales for store " + i + " :");
+            int sales = scanner.nextInt();
+            
+            String stars = "";
+            for (int f = sales; f >= 100; f -= 100){
+                stars += "*";
+            }
+            
+            chart += "Store " + i + " : " + stars + "\n";
+        }
+        
+        System.out.print(chart);
+        
+        scanner.close();
+    }
 }
