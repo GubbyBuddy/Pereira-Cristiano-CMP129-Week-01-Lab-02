fcfc564d6d017c7c8aa550c933a2729ff06d386e
Gubbybuddy <pereira.cristiano@student.ccm.edu>
2026-09-11 14:28:28 -0400

    commit 2

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,8 +1,18 @@
-package Labs.Week_1.scr;
+import java.util.Scanner;
 
 public class SalesBarChart {
-    public static void main(String []args){
-        
-
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String chart = ""; 
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Please enter todays sales for store " + i + " :");
+            int sales = scanner.nextInt();
+            String stars = "";
+            for (int f = sales; f >= 100; f -= 100){
+                stars += "*";
+            }
+            chart += "Store " + i + " : " + stars + "\n";
+        }
+        System.out.print(chart);
+            }
 }
